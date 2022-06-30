2a34edf4fa8827ad2eabfa428c09dca2b885f012
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-06-30 20:29:20 -0300

    primera instancia

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+    return(
+      <>
+        <h1 className='text-6xl font-extrabold'>CRM Con React</h1>
+      </>
+    )
+
+}
+
+export default App
