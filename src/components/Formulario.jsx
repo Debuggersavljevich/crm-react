6600d915ee04cad6e10139e951b86bac494b4058
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-07-02 01:58:46 -0300

    se agrego routers y links

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <div className='bg-teal-700 mt-10 px-5 py-10 rounded-md shadow-2xl md:w-3/4 mx-auto'>
+        <h1 className='text-white font-bold texl-xl text-center uppercase'>Agregar Cliente</h1>
+    </div>
+  )
+}
+
+export default Formulario
--- src/paginas/NuevoCliente.jsx
+++ src/paginas/NuevoCliente.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Formulario from '../components/Formulario'
 
 const NuevoCliente = () => {
   return (
     <>
-        <h1>NuevoCliente.jsx</h1>
+        <h1 className='font-black text-4xl text-teal-700'>Nuevo cliente</h1>
+        <p className='mt-3'>Llená la información del cliente</p>
+        <Formulario></Formulario>
     </>
   )
 }
